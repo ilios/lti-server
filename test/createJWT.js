c4207a537e226504fc01b1105479b7f65753a353
Jonathan Johnson <jon.johnson@ucsf.edu>
2017-05-23 15:52:51 -0700

    Add createJWT library
    This will generate short usage tokens with the data necessary to
    authenticate the lti-app.

---
+++ test/createJWT.js
@@ -0,0 +1,41 @@
+'use strict';
+
+const createJWT = require('../lib/createJWT');
+const jwt = require('jsonwebtoken');
+const assert = require('assert');
+const moment = require('moment');
+
+describe('Generates a JWT from provided data', function() {
+  const userId = 24;
+  const apiHost = 'https://example.com';
+  const apiNameSpace = '/api/v1';
+  const secret = 'secret';
+  const token = createJWT(userId, apiHost, apiNameSpace, secret);
+
+  assert.ok(token.length > 200);
+  const obj = jwt.decode(token);
+  it('has the right passed values', function() {
+    assert.equal(obj.iss, 'ilios-lti-server');
+    assert.equal(obj.aud, 'ilios-lti-app');
+
+    const expiresAt = moment(obj.exp, 'X');
+    const now = moment();
+    const diff = expiresAt.diff(now, 's');
+    assert.ok(diff < 61);
+  });
+  it('has the right default values', function() {
+    assert.equal(obj.user_id, userId);
+    assert.equal(obj.apiHost, apiHost);
+    assert.equal(obj.apiNameSpace, apiNameSpace);
+  });
+  it('expires in less than 60 seconds', function() {
+    const expiresAt = moment(obj.exp, 'X');
+    const now = moment();
+    const diff = expiresAt.diff(now, 's');
+    assert.ok(diff < 61);
+  });
+  it('is valid', function() {
+    const isValid = jwt.verify(token, 'ilios.jwt.key.' + secret);
+    assert.ok(isValid);
+  });
+});
