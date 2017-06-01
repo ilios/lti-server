09a2dd6e929da8399e6f2e878811db2bd1543bc3
Jonathan Johnson <jon.johnson@ucsf.edu>
2017-05-31 19:27:08 -0700

    Read school config from JSON stored in S3
    School configuration is stored in an AWS S3 bucket and read from there.

--- test/readSchoolConfigTest.js
+++ test/readSchoolConfigTest.js
@@ -1,44 +1,62 @@
 'use strict';
 
 const readSchoolConfig = require('../lib/readSchoolConfig');
+const fs = require('fs');
 const assert = require('assert');
+
 describe('Read the configuration for a school', function() {
-  const obj = require('./sample-config.json');
-  it('reads the first school correctly', function() {
-    const result = readSchoolConfig('demo-school-config', obj);
+  const json = fs.readFileSync(`${__dirname}/sample-config.json`);
+  const S3 = function(){
+    this.getObject = () => {
+      return {
+        async promise(){
+          return {
+            Body: json
+          };
+        }
+      };
+    };
+  };
+  const aws = { S3 };
+  it('reads the first school correctly', async function() {
+    const result = await readSchoolConfig('demo-school-config', aws);
     assert.ok('apiServer' in result, 'result contains apiServer');
     assert.equal(result.apiServer, 'https://test-ilios.com', 'apiServer is correct');
     assert.ok('apiNameSpace' in result, 'result contains apiNameSpace');
     assert.equal(result.apiNameSpace, '/test/api/v1', 'apiNameSpace is correct');
     assert.ok('ltiUserId' in result, 'result contains ltiUserId');
     assert.equal(result.ltiUserId, 33, 'ltiUserId is correct');
-    assert.ok('secret' in result, 'result contains secret');
-    assert.equal(result.secret, 'secret123', 'secret is correct');
+    assert.ok('consumerSecret' in result, 'result contains consumerSecret');
+    assert.equal(result.consumerSecret, 'secret123', 'consumerSecret is correct');
+    assert.ok('iliosSecret' in result, 'result contains iliosSecret');
+    assert.equal(result.iliosSecret, 'secret123', 'iliosSecret is correct');
     assert.ok('ltiPostField' in result, 'result contains ltiPostField');
     assert.equal(result.ltiPostField, 'ext_user_username', 'ltiPostField is correct');
     assert.ok('iliosMatchField' in result, 'result contains iliosMatchField');
     assert.equal(result.iliosMatchField, 'authentication-username', 'iliosMatchField is correct');
   });
-  it('reads the second school correctly', function() {
-    const result = readSchoolConfig('second-school-config', obj);
+  it('reads the second school correctly', async function() {
+    const result = await readSchoolConfig('second-school-config', aws);
     assert.ok('apiServer' in result, 'result contains apiServer');
     assert.equal(result.apiServer, 'https://second-test-ilios.com', 'apiServer is correct');
     assert.ok('apiNameSpace' in result, 'result contains apiNameSpace');
     assert.equal(result.apiNameSpace, '/api/v1', 'apiNameSpace is correct');
     assert.ok('ltiUserId' in result, 'result contains ltiUserId');
     assert.equal(result.ltiUserId, 11, 'ltiUserId is correct');
-    assert.ok('secret' in result, 'result contains secret');
-    assert.equal(result.secret, 'secret456!', 'secret is correct');
+    assert.ok('consumerSecret' in result, 'result contains consumerSecret');
+    assert.equal(result.consumerSecret, 'secret456!', 'consumerSecret is correct');
+    assert.ok('iliosSecret' in result, 'result contains iliosSecret');
+    assert.equal(result.iliosSecret, 'abcd', 'iliosSecret is correct');
     assert.ok('ltiPostField' in result, 'result contains ltiPostField');
     assert.equal(result.ltiPostField, 'ext_user_username', 'ltiPostField is correct');
     assert.ok('iliosMatchField' in result, 'result contains iliosMatchField');
     assert.equal(result.iliosMatchField, 'authentication-username', 'iliosMatchField is correct');
   });
-  it('dies well when a bad config is requested', function() {
+  it('dies well when a bad config is requested', async function() {
     try {
-      readSchoolConfig('bad-school-config', obj);
+      await readSchoolConfig('bad-school-config', aws);
     } catch(e) {
-      assert.equal(e, 'Error: "bad-school-config" is not known to Ilios. Please contact support@iliosproject.org to set it up.');
+      assert.equal(e, 'The Consumer Key "bad-school-config" is not known to Ilios. Please contact support@iliosproject.org to set it up.');
     }
   });
 });
