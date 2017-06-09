180b9711a537d96acef6d86a1ae30b2223bff870
Jonathan Johnson <jon.johnson@ucsf.edu>
2017-06-09 13:14:03 -0700

    Cache userId with Redis
    This doesn't actually work because we can't connect to ElasticCache from
    our lambda endpoint without using a VPC, but I wanted to save the
    working code in case we end up needing to go down this route.

--- test/findIliosUserTest.js
+++ test/findIliosUserTest.js
@@ -15,7 +15,18 @@ describe('Get the ID for a user', function() {
   const searchString = 'test-username';
 
   const createJWT = (id) => `TOKEN${id}`;
-  it('calls the api and returns a userId', async function() {
+  const keyHash = `${config.apiServer}:${config.apiNameSpace}:${config.ltiPostField}:${config.iliosMatchField}:userId:${searchString}`;
+  it('calls the api and returns a userId when there is no data in the cache', async function() {
+    const Redis = function(){
+      this.get = (key) => {
+        assert.equal(key, keyHash);
+        return Promise.resolve(null);
+      };
+      this.set = (key, value) => {
+        assert.equal(key, keyHash);
+        assert.equal(value, 24);
+      };
+    };
     const fetch = async (url) => {
       assert.equal(url, `${config.apiServer}${config.apiNameSpace}/authentications?filters[username]=${searchString}`);
       return {
@@ -28,10 +39,27 @@ describe('Get the ID for a user', function() {
         }
       };
     };
-    const result = await findIliosUser({fetch, createJWT, config, searchString});
+    const result = await findIliosUser({fetch, createJWT, config, searchString, Redis});
     assert.equal(result, 24);
   });
+  it('Users the id in the cache when it exists', async function() {
+    const Redis = function(){
+      this.get = (key) => {
+        assert.equal(key, keyHash);
+        return Promise.resolve(11);
+      };
+    };
+    const fetch = null;
+    const result = await findIliosUser({fetch, createJWT, config, searchString, Redis});
+    assert.equal(result, 11);
+  });
   it('dies well when a bad search is performed', async function() {
+    const Redis = function(){
+      this.get = (key) => {
+        assert.equal(key, keyHash);
+        return Promise.resolve(null);
+      };
+    };
     const fetch = async () => {
       return {
         json(){
@@ -42,7 +70,7 @@ describe('Get the ID for a user', function() {
       };
     };
     try {
-      await findIliosUser({fetch, createJWT, config, searchString});
+      await findIliosUser({fetch, createJWT, config, searchString, Redis});
     } catch(e) {
       assert.equal(e, `Unable to find Ilios account for ${searchString}.`);
     }
