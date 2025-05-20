989ece67ea1ff7b7bc250aede2872266e7070b98
Stefan Topfstedt <Stefan.Topfstedt@ucsf.edu>
2025-05-20 13:55:06 -0700

    fix formatting by adding a blank line after import statements.

--- test/launchDashboardTest.js
+++ test/launchDashboardTest.js
@@ -1,5 +1,6 @@
 import launchDashboard from '../lib/launchDashboard.js';
 import assert from 'assert';
+
 describe('Dashboard Response handler works', function() {
   it('sends a redirect with all the right data', async function () {
     const ltiRequestValidator = () => true;
