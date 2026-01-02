043351a800d7ac9cd186b99495a7f9d1d4d2bc2f
Stefan Topfstedt <Stefan.Topfstedt@ucsf.edu>
2026-01-02 09:11:40 -0800

    make eslint aware of node globals.
    this is needed for the husky configuration, which references/uses node
    globals such as "process" and "console".

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,4 +1,5 @@
 import eslint from '@eslint/js';
+import globals from 'globals';
 import tseslint from 'typescript-eslint';
 import { defineConfig, globalIgnores } from 'eslint/config';
 import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended';
@@ -6,6 +7,7 @@ export default defineConfig([
   globalIgnores(['node_modules', '.aws-sam']),
   {
     languageOptions: {
+      globals: globals.node,
       parserOptions: {
         ecmaVersion: 2020,
         sourceType: 'module',
